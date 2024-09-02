3587cc77e4086636eb50d428b68fd3b188087ec2
techarenz <57818506+techarenz@users.noreply.github.com>
2024-09-02 11:44:15 +0800

    Update Main.java
    Added Academic Honesty pledge.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-// I have neither given nor received unauthorized aid on this piece of work.
+/**
+*****************************************************************************************************
+* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
+* “I have neither given nor received unauthorized aid on this piece of work.”
+* NAME: 
+*****************************************************************************************************
+*/
 
 public class Main {
 
